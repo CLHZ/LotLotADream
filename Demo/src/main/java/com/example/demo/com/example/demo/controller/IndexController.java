7049251a76ac7f0709e63b20d5df807304ca0e67
Raineys <2926601072@qq.com>
2018-12-05 17:03:17 +0800

    第三次提交

--- Demo/src/main/java/com/example/demo/com/example/demo/controller/IndexController.java
+++ Demo/src/main/java/com/example/demo/com/example/demo/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
     public String one(Model model) {
         System.out.println("=========执行==========");
         // 往model中放数据
-        model.addAttribute("score",18);
+        model.addAttribute("score",20);
         model.addAttribute("name","何婷华");
         model.addAttribute("sex","女");
         return "one";
